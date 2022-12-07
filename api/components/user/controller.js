bb4f547bbe17a4e0488f4059aaf1c0377f547794
KevinArce <iav.kevinarce@ufg.edu.sv>
2022-12-06 21:24:12 -0600

    added auth for users

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,4 +1,5 @@
-const nanoid = require("nanoid");
+const { nanoid } = require("nanoid")
+const auth = require("../auth");
 
 const TABLE = "user";
 
@@ -16,7 +17,7 @@ module.exports = function (injectedStore) {
     return store.get(TABLE, id);
   }
 
-  function upsert(body) {
+  async function upsert(body) {
     const user = {
       name: body.name,
       username: body.username,
@@ -26,6 +27,14 @@ module.exports = function (injectedStore) {
       user.id = body.id;
     } else {
       user.id = nanoid();
+    }
+
+    if (body.password || body.username) {
+      await auth.upsert({
+        id: user.id,
+        username: user.username,
+        password: body.password,
+      });
     }
 
     return store.upsert(TABLE, user);
